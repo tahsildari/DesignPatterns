4491b8daccfeb597de1f75ebe698596fa346aa73
MTahsildari <MTahsildari@TQ0259>
2022-11-22 02:30:16 +0100

    Rules Engine: pass first purchase test

---
+++ DesignPatternsAsISeeThem/Behavorial/RulesEngine/Context/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsAsISeeThem.Behavorial.RulesEngine.Context
+{
+    public class Customer
+    {
+        public DateTime Birthdate { get; set; }
+        public DateTime? DateOfFirstPurchase { get; set; }
+        public bool IsTeacher { get; set; }
+    }
+}
---
+++ DesignPatternsAsISeeThem/Behavorial/RulesEngine/DiscountCalculator.cs
@@ -0,0 +1,26 @@
+﻿using DesignPatternsAsISeeThem.Behavorial.RulesEngine.Context;
+using System;
+using System.Collections.Specialized;
+
+namespace DesignPatternsAsISeeThem.Behavorial.RulesEngine
+{
+    public class DiscountCalculator
+    {
+        public DiscountCalculator()
+        {
+        }
+
+        // Rules:
+        // First purchase has 15% discount for everyone
+        // Each year has 1% discount up to a 10% maximum, for teachers 2-12%
+        // Purchases on birthday have 10% extra discount
+        // On teacher's day, teachers get 7% extra discount
+        public decimal Calculate(Customer customer)
+        {
+            if (customer.DateOfFirstPurchase == null)
+                return .15m;
+            else
+                return 0;
+        }
+    }
+}
