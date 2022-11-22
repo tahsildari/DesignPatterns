624a69b8d0732907b7e79a535e453a7316e00c56
MTahsildari <MTahsildari@TQ0259>
2022-11-22 20:59:16 +0100

    Rules Engine: Pulled out each rule group to its own method

--- DesignPatternsAsISeeThem/Behavorial/RulesEngine/DiscountCalculator.cs
+++ DesignPatternsAsISeeThem/Behavorial/RulesEngine/DiscountCalculator.cs
@@ -16,6 +16,63 @@ namespace DesignPatternsAsISeeThem.Behavorial.RulesEngine
             TeachersDay = worldsTeachersDay;
         }
 
+        public decimal CalculateDiscountForFirstTimeCustomer(Customer customer)
+        {
+            if (customer.DateOfFirstPurchase == null)
+                return .15m;
+            return 0m;
+        }
+
+        public decimal CalculateDiscountForLoyalCustomers(Customer customer)
+        {
+            if (customer.DateOfFirstPurchase.HasValue)
+            {
+                if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-10))
+                    return .10m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-9))
+                    return .09m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-8))
+                    return .08m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-7))
+                    return .07m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-6))
+                    return .06m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-5))
+                    return .05m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-4))
+                    return .04m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-3))
+                    return .03m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-2))
+                    return .02m;
+                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-1))
+                    return .01m;
+            }
+            return 0m;
+        }
+
+        public decimal CalculateDiscountForTeacherCustomers(Customer customer, decimal percent)
+        {
+            if (customer.DateOfFirstPurchase.HasValue)
+            {
+                bool isItTeachersDay =
+                    TeachersDay.Day == DateTime.Today.Day &&
+                    TeachersDay.Month == DateTime.Today.Month;
+
+                if (customer.IsTeacher)
+                    percent += (isItTeachersDay ? .07m : .02m);
+            }
+            return percent;
+        }
+
+        public decimal CalculateDiscountForCustomersBirthday(Customer customer, decimal percent)
+        {
+            if (customer.Birthdate.Month == DateTime.Now.Month
+                && customer.Birthdate.Day == DateTime.Now.Day)
+                percent += .10m;
+            return percent;
+        }
+
         // Rules:
         // First purchase has 15% discount for everyone
         // Each year has 1% discount up to a 10% maximum, for teachers 2% more
@@ -25,42 +82,10 @@ namespace DesignPatternsAsISeeThem.Behavorial.RulesEngine
         {
             var percent = 0m;
 
-            if (customer.DateOfFirstPurchase == null)
-                percent = .15m;
-            else
-            {
-                if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-10))
-                    percent = .10m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-9))
-                    percent = .09m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-8))
-                    percent = .08m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-7))
-                    percent = .07m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-6))
-                    percent = .06m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-5))
-                    percent = .05m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-4))
-                    percent = .04m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-3))
-                    percent = .03m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-2))
-                    percent = .02m;
-                else if (customer.DateOfFirstPurchase.Value < DateTime.Now.AddYears(-1))
-                    percent = .01m;
-
-                bool isItTeachersDay =
-                    TeachersDay.Day == DateTime.Today.Day &&
-                    TeachersDay.Month == DateTime.Today.Month;
-
-                if (customer.IsTeacher)
-                    percent += (isItTeachersDay ? .07m : .02m);
-            }
-
-            if (customer.Birthdate.Month == DateTime.Now.Month
-                && customer.Birthdate.Day == DateTime.Now.Day)
-                percent += .10m;
+            percent = Math.Max(CalculateDiscountForFirstTimeCustomer(customer), percent);
+            percent = Math.Max(CalculateDiscountForLoyalCustomers(customer), percent);
+            percent = Math.Max(CalculateDiscountForTeacherCustomers(customer, percent), percent);
+            percent = Math.Max(CalculateDiscountForCustomersBirthday(customer, percent), percent);
 
             return percent;
         }
