f5f947567ebffbd019525741ad70403e1d77b7d5
MTahsildari <MTahsildari@TQ0259>
2022-11-22 02:39:07 +0100

    Rules Engine: teachers years of membership plus 2 percent test > failing

--- Tests/DiscountCalculatorRulesEngineTests.cs
+++ Tests/DiscountCalculatorRulesEngineTests.cs
@@ -32,7 +32,33 @@ namespace Tests
         [InlineData(9,9)]
         [InlineData(10,10)]
         [InlineData(15,10)]
-        public void ReturnPercentEqualToYearsOfMembershipForNonTeachersForFirst10Years(int year, decimal expectedPercent)
+        public void ReturnPercentEqualToYearsOfMembershipForNonTeachersForForMaxOf10(int year, decimal expectedPercent)
+        {
+            Customer customer = new Customer
+            {
+                DateOfFirstPurchase = DateTime.Now.AddYears(-1 * year),
+                IsTeacher = false
+            };
+
+            var sut = new DiscountCalculator();
+            var percent = sut.Calculate(customer);
+
+            percent.Should().Be(expectedPercent);
+        }
+
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(2, 4)]
+        [InlineData(3, 5)]
+        [InlineData(4, 6)]
+        [InlineData(5, 7)]
+        [InlineData(6, 8)]
+        [InlineData(7, 9)]
+        [InlineData(8, 10)]
+        [InlineData(9, 11)]
+        [InlineData(10, 12)]
+        [InlineData(15, 12)]
+        public void ReturnPercentEqualToYearsOfMembershipPlusTwoForTeachersForMaxOf12(int year, decimal expectedPercent)
         {
             Customer customer = new Customer
             {
